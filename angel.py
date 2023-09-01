b2a1abadfcb5f7dae1b957d1a19579da65e1202a
Angel199610 <kwagalaangel27@gmail.com>
2023-09-01 04:38:02 -0700

    index

---
+++ angel.py
@@ -0,0 +1,36 @@
+#the following are floats
+#print(10.953)
+#print(8.0)
+
+#Out put bool
+#print("")
+#print("There's a snake in my boot")
+#print(True)
+
+#Output my name
+#print("")
+#print(True)
+
+#integer(int)
+num1 = 5
+num2 = 8.4
+#print(num1, num2)
+
+#Integers (int)additional work
+John = 4
+Raum = 9
+Wonder = 12
+total_amount = ("John, Raum, Wonder")
+#print(total_amount)
+
+#re-declaring num to store an integer
+num = "9"
+num = int(num)
+#print(type(num))
+
+def myfunc():
+  global x
+  x = "fantastic"
+
+myfunc()
+print("Python is " + x)
---
+++ index.py
@@ -0,0 +1,37 @@
+#Integer(int)
+tall_men = 20
+total_amount = 300
+newTotalAmount = str(total_amount)
+total = int(newTotalAmount)
+#Type casting integers
+#print(type(newTotalAmount)) #int to str
+print(type(total))
+
+#string(str)
+currency = "UGX"
+payment = "Your payment is " + currency + " " + str(total_amount)
+Message = """ 
+  Type casting is about converting a variable data type to a string
+   """
+
+
+#the out put
+#print(payment)
+#print("Your payment is UGX " + str(total_amount))
+
+#Booleans (bool)
+IsMoving = True
+IsHanging = False
+
+#OutPut
+if IsMoving : True
+#print(IsHanging)
+
+#strings are array like structure meaning the first character of the array starts with zero
+Fruit = "banana"
+print(Fruit[0])
+for a in Fruit:
+    print(a) 
+
+
+
